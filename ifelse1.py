4e69f5472560d735860760f2953d941b871d7803
premyennam <128230752+premyennam@users.noreply.github.com>
2023-04-10 19:15:59 +0530

    Add files via upload

---
+++ ifelse1.py
@@ -0,0 +1,5 @@
+n = int(input().strip())
+if (n%2!=0) or (n>=6 and n<=20):
+    print("Weird")
+else:
+    print("Not Weird")
--- print.py
+++ print.py
@@ -1,1 +1,3 @@
-print("Hello World")
+n = int(input())
+for i in range(1,n+1):
+    print(i,end="")
